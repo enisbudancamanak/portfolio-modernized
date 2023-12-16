a8fee3310782e357d5f3d5dfa710bfb8c1c67473
Enis Budancamanak <Enis.Budancamanak@hotmail.com>
2023-12-16 13:32:34 +0100

    Updates

---
+++ _app/immutable/chunks/navigation.We8AAPIM.js
@@ -0,0 +1,1 @@
+import{j as a}from"./singletons.m21f6WzI.js";const o=a("goto"),e=a("on_navigate"),n=a("after_navigate");export{n as a,o as g,e as o};
--- _app/immutable/chunks/navigation.yV9gYDoY.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./singletons.2umLUnpe.js";const o=a("goto"),e=a("on_navigate"),n=a("after_navigate");export{n as a,o as g,e as o};
